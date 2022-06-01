33f6ab9071c9403ca5846fd4eaf357a2b0b330ae
Idris Dev <IdrisDose@users.noreply.github.com>
2022-06-01 17:15:09 +1000

    feat: Added more robust checks and refund.
    I made the checks more robust and refunded if you could not add item (over-encumbered, not enough slots etc).

--- server/main.lua
+++ server/main.lua
@@ -4,13 +4,21 @@ RegisterNetEvent('qb-robberylist:server:buyshit', function(ped)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     if not src or not Player or not ped then return end
-    local cash = Player.PlayerData.money[Config.Shop[ped].type]
+    -- local cash = Player.PlayerData.money[Config.Shop[ped].type]
+    local price = Config.Shop[ped].price
+    local payType = Config.Shop[ped].type
 
-    if cash >= Config.Shop[ped].price then
-        Player.Functions.RemoveMoney(Config.Shop[ped].type, Config.Shop[ped].price)
-        Player.Functions.AddItem(Config.Shop[ped].item, 1)
-        TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.Shop[ped].item], "add")
-    else
-        TriggerClientEvent('QBCore:Notify', src, 'Not enough '..Config.Shop[ped].type, 'error')
+    if Player.Functions.RemoveMoney(payType, price) ~= true then
+        TriggerClientEvent('QBCore:Notify', src, 'Not enough ' .. payType, 'error')
+        return
     end
+
+    if Player.Functions.AddItem(Config.Shop[ped].item, 1) ~= true then
+        Player.Functions.AddMoney(payType, price)
+        TriggerClientEvent('QBCore:Notify', src, 'Could not give item', 'error')
+        return
+    end
+
+    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.Shop[ped].item], "add")
+    TriggerClientEvent('QBCore:Notify', src, "Here's the contraband, Good Luck!")
 end)
